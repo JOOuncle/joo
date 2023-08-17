36500ea128a0755dadcaf7d5128d1306b87724ac
JOOuncle <kjhkor90@gmail.com>
2023-08-17 23:55:44 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 const express = require(`express`);
 const app = express();
 const port = 5000;
+const bodyParser = require(`body-parser`);
+const { User } = require(`./models/User`);
+
+const config = require(`./config/key`);
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require(`mongoose`);
 
 mongoose
-  .connect(
-    `mongodb+srv://JOOuncle:abcd1234@cluster0.opip0rw.mongodb.net/?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreatindex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreatindex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log(`MongoDB Connected...`))
   .catch((err) => console.log(err));
 
 app.get(`/`, (req, res) => res.send(`Hello World!`));
+
+app.post(`/register`, (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  }); // mongoDB method
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
