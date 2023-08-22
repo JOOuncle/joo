1e46609294dcda62e7552d56cf8f1e71b24b00cb
JOOuncle <kjhkor90@gmail.com>
2023-08-23 00:14:46 +0900

    토큰 생성

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require(`express`);
 const app = express();
 const port = 5000;
 const bodyParser = require(`body-parser`);
-const { User } = require(`./models/User`);
-
+const cookieParser = require(`cookie-parser`);
 const config = require(`./config/key`);
+
+const { User } = require(`./models/User`);
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require(`mongoose`);
 
@@ -34,6 +36,32 @@ app.post(`/register`, (req, res) => {
       success: true,
     });
   }); // mongoDB method
+});
+
+app.post(`/login`, (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
