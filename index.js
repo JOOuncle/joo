f109364a55c906e900c219e90b9fce46dee591eb
JOOuncle <kjhkor90@gmail.com>
2023-08-26 02:00:21 +0900

    토큰 인증 & 로그아웃

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 5000;
 const bodyParser = require(`body-parser`);
 const cookieParser = require(`cookie-parser`);
 const config = require(`./config/key`);
-
+const { auth } = require(`./middleware/auth`);
 const { User } = require(`./models/User`);
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,7 +27,7 @@ mongoose
 
 app.get(`/`, (req, res) => res.send(`Hello World!`));
 
-app.post(`/register`, (req, res) => {
+app.post(`/api/users/register`, (req, res) => {
   const user = new User(req.body);
 
   user.save((err, userInfo) => {
@@ -38,7 +38,7 @@ app.post(`/register`, (req, res) => {
   }); // mongoDB method
 });
 
-app.post(`/login`, (req, res) => {
+app.post(`/api/users/login`, (req, res) => {
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
       return res.json({
@@ -60,6 +60,28 @@ app.post(`/login`, (req, res) => {
           .status(200)
           .json({ loginSuccess: true, userId: user._id });
       });
+    });
+  });
+});
+
+app.get(`/api/users/auth`, auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get(`/api/users/logout`, auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
     });
   });
 });
